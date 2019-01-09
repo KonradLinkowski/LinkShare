46e6c64b9ab0eaddcb8a30ba346057f9a0573446
Konrad Linkowski <linkowski.konrad97@gmail.com>
2019-01-09 22:00:22 +0100

    WIP - facebook and google login

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import { OAuthCredential } from '@firebase/auth-types';
+import { exists } from 'fs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,83 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LinkShare';
+  onClickMe() {
+    const db = firebase.firestore();
+    db.settings({
+      timestampsInSnapshots: true
+    });
+    db.collection('test').add({
+      name: 'test',
+      test: 323
+    })
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+  private signWithProvider(provider: firebase.auth.AuthProvider) {
+    firebase.auth().signInWithPopup(provider).then(result => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = (<OAuthCredential>result.credential).accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+      console.log(user)
+      const db = firebase.firestore();
+      db.settings({
+        timestampsInSnapshots: true
+      });
+      const doc = db.collection('users').doc(user.uid)
+      doc.get()
+      .then(res => {
+        if (!res.exists) {
+          doc.set({
+            displayName: user.displayName
+          })
+          .catch(error => {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // ...
+        });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      })
+    }).catch(error => {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+      console.log(errorCode, errorMessage);
+    });
+  }
+  google() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    this.signWithProvider(provider)
+  }
+  facebook() {
+    var provider = new firebase.auth.FacebookAuthProvider();
+    this.signWithProvider(provider)
+  }
+  signout() {
+    firebase.auth().signOut().then(function() {
+      // Sign-out successful.
+      console.log('logged out')
+    }).catch(function(error) {
+      // An error happened.
+      console.log('errored')
+    });
+  }
 }
--- Angular/src/main.ts
+++ Angular/src/main.ts
@@ -4,9 +4,20 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import * as firebase from 'firebase/app';
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAyjOGVsKWcX_zeaeuChwBBo6e_QeKq4LM",
+  authDomain: "linkshare-ec755.firebaseapp.com",
+  databaseURL: "https://linkshare-ec755.firebaseio.com",
+  projectId: "linkshare-ec755",
+  storageBucket: "linkshare-ec755.appspot.com",
+  messagingSenderId: "674524310907"
+});
