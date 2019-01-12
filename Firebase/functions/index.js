4a85fedb9d8102eac1214b5f6c2ebf1928e75b14
Konrad Linkowski <linkowski.konrad97@gmail.com>
2019-01-12 22:58:49 +0100

    Update firestore rules and add remove all data function

--- Firebase/functions/index.js
+++ Firebase/functions/index.js
@@ -1,8 +1,30 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+const db = admin.firestore();
+
+exports.deleteAllUserData = functions.auth.user().onDelete(user => {
+  return new Promise((resolve, reject) => {
+    db.collection('links').where('owner', '==', user.uid).get()
+    .then(res => {
+      const batch = db.batch();
+      res.docs.forEach(doc => {
+        batch.delete(doc.ref);
+      })
+      batch.commit();
+      db.collection('users').doc(user.uid).delete()
+      .then(() => {
+        resolve();
+      })
+      .catch(err => {
+        console.log(err);
+        reject(err);
+      })
+    })
+    .catch(err => {
+      console.log(err);
+      reject(err);
+    })
+  })
+});
