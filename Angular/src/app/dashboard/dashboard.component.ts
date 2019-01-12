95cbdc509bb0495cfbd712fb47261ed6bedb6d9a
Konrad Linkowski <linkowski.konrad97@gmail.com>
2019-01-12 22:59:50 +0100

    Add delete account button

--- Angular/src/app/dashboard/dashboard.component.ts
+++ Angular/src/app/dashboard/dashboard.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Link } from '../link';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
-
+export class DashboardComponent implements OnInit, OnDestroy {
   links: Link[];
+  private linksValueChanges: Subscription;
 
   constructor(private fireAuth: AngularFireAuth, private firestore: AngularFirestore) { }
 
@@ -19,7 +20,11 @@ export class DashboardComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.firestore.collection<Link>('links', ref => ref.where('owner', '==', this.fireAuth.auth.currentUser.uid))
+    this.linksValueChanges = this.firestore.collection<Link>('links', ref => ref.where('owner', '==', this.fireAuth.auth.currentUser.uid))
       .valueChanges().subscribe(links => this.links = links);
+  }
+
+  ngOnDestroy() {
+    this.linksValueChanges.unsubscribe();
   }
 }
--- Angular/src/app/settings/settings.component.ts
+++ Angular/src/app/settings/settings.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-settings',
@@ -7,7 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fireAuth: AngularFireAuth, private router: Router, private firestore: AngularFirestore) { }
+
+  deleteAccount() {
+    const user = this.fireAuth.auth.currentUser;
+    user.delete()
+    .then(() => {
+      this.router.navigateByUrl('login');
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }
 
   ngOnInit() {
   }
