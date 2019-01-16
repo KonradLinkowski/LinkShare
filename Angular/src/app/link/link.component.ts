b575c98fe9ea2edbfbf7b667cdf09ddf60ed0182
Konrad Linkowski <linkowski.konrad97@gmail.com>
2019-01-16 23:02:53 +0100

    X button if is Mine

--- Angular/src/app/link/link.component.ts
+++ Angular/src/app/link/link.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Link } from '../models/link';
 import { DomSanitizer } from '@angular/platform-browser';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-link',
@@ -12,8 +13,11 @@ export class LinkComponent implements OnInit {
 
   @Input() link: Link;
 
-  constructor(private sanitizer: DomSanitizer, private firestore: AngularFirestore) {}
+  constructor(private sanitizer: DomSanitizer, private firestore: AngularFirestore, private fireAuth: AngularFireAuth) {}
 
+  isMine() {
+    return this.fireAuth.auth.currentUser && this.fireAuth.auth.currentUser.uid === this.link.owner;
+  }
 
   deleteLink() {
     this.firestore.collection('folders').doc(this.link.folder).collection('links').doc(this.link.id).delete()
