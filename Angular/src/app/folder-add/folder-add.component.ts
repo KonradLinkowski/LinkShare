ec172c4aefd074e1ababc022a13b932e22a65633
Konrad Linkowski <linkowski.konrad97@gmail.com>
2019-01-19 16:45:21 +0100

    Improve css

--- Angular/src/app/folder-add/folder-add.component.ts
+++ Angular/src/app/folder-add/folder-add.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireAuth } from '@angular/fire/auth';
@@ -9,6 +9,7 @@ import { AngularFireAuth } from '@angular/fire/auth';
   styleUrls: ['./folder-add.component.css']
 })
 export class FolderAddComponent implements OnInit {
+  @Input() callback: (error?: firebase.firestore.DocumentReference) => void;
   folderForm = new FormGroup({
     name: new FormControl('')
   });
@@ -21,10 +22,10 @@ export class FolderAddComponent implements OnInit {
       owner: this.fireAuth.auth.currentUser.uid
     })
     .then(() => {
-      console.log();
+      this.callback();
     })
     .catch(err => {
-      console.log(err);
+      this.callback(err);
     });
     this.folderForm.setValue({
       name: ''
